b2ac317cd45f334d3c6ba1951ea00f988589f0c3
Martynas Dargis <dargis.martynas@gmail.com>
2016-07-18 22:43:36 +0300

    initial project

---
+++ project/app/src/main/java/mabeproductions/app/smartereveryday/DailyFactActivity.java
@@ -0,0 +1,13 @@
+package mabeproductions.app.smartereveryday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DailyFactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_fact);
+    }
+}
